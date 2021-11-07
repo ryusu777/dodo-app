c6cbde595399752b1c5064a950dc94db86c8c5fa
Veronica Yose Ardilla <veronica.ardilla@student.matanauniversity.ac.id>
2021-11-07 21:01:38 +0700

    Page Navigation Menu

--- dodo-front-end/src/layouts/MainLayout.vue
+++ dodo-front-end/src/layouts/MainLayout.vue
@@ -12,10 +12,8 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+         Dodo App
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -25,17 +23,9 @@
       bordered
     >
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item v-for="data in menu" :key="data.id"> 
+          <q-item-section><a :href="data.url" class="text-black" style="text-decoration: none">{{ data.text }}</a></q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
@@ -46,52 +36,8 @@
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+
 
 import { defineComponent, ref } from 'vue'
 
@@ -99,14 +45,50 @@ export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
   },
-
+  data(){
+    return{
+      menu: [
+        {
+          id: 1,
+          url: '/#/create',
+          text: 'Tambah Barang'
+        },
+        {
+          id: 2,
+          url: '/#/update',
+          text: 'Ubah Barang'
+        },
+        {
+          id: 3,
+          url: '/#/view',
+          text: 'Lihat Barang'
+        },
+      ],
+    }
+  },
   setup () {
     const leftDrawerOpen = ref(false)
 
+    // const menu = [
+    //   {
+    //     id: 1,
+    //     title: 'Tambah Barang',
+    //     link: '/create'
+    //   },
+    //   {
+    //     id: 2,
+    //     title: 'Ubah Barang',
+    //     link: '/update'
+    //   },
+    //   {
+    //     id: 3,
+    //     title: 'Lihat Barang',
+    //     link: '/view'
+    //   },
+    // ];
     return {
-      essentialLinks: linksList,
+      // menu,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
--- dodo-front-end/src/router/routes.ts
+++ dodo-front-end/src/router/routes.ts
@@ -8,7 +8,8 @@ const routes: RouteRecordRaw[] = [
       { path: '', component: () => import('pages/Index.vue') },
       { path: 'create', component: () => import('src/pages/crud-pages/CreateGoods.vue') },
       { path: 'update', component: () => import('src/pages/crud-pages/UpdateGoods.vue') },
-      { path: 'view', component: () => import('src/pages/crud-pages/ReadGoods.vue') }
+      { path: 'view', component: () => import('src/pages/crud-pages/ReadGoods.vue') },
+      { path: 'coba', component: () => import('src/pages/crud-pages/coba.vue') }
     ],
   },
 
